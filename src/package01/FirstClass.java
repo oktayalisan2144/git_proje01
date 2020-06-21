fb1424bede897d172e69d38e2a763fe2a380e1f5
your name <ihcan.21@hotmail.com>
2020-06-21 22:14:36 +0300

    ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -3,7 +3,8 @@ package package01;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Ilk commit from local repository");
+		System.out.println("Ikinci commit from local repository");
 
 	}
 
