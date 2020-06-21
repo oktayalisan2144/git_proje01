ef1e11befb75349629d9f11833f23ae78e81649a
your name <ihcan.21@hotmail.com>
2020-06-21 22:33:31 +0300

    feature branc ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Ilk commit from local repository");
 		System.out.println("Ikinci commit from local repository");
+		
+		System.out.println("Yeni branch olusturdum ve adi feature");
 
 	}
 
